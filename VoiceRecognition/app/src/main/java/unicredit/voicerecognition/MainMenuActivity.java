1626d98736656d1cd791bb2d49c627da9311b03f
Alex Filiuta <alex.filiuta@gmail.com>
2016-08-02 00:26:07 +0300

    menu functionality (voice to text)

--- VoiceRecognition/app/src/main/java/unicredit/voicerecognition/MainMenuActivity.java
+++ VoiceRecognition/app/src/main/java/unicredit/voicerecognition/MainMenuActivity.java
@@ -1,5 +1,6 @@
 package unicredit.voicerecognition;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,5 +11,10 @@ public class MainMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+    }
+
+    public void startVoiceText(View view) {
+        Intent intent = new Intent(this, VoiceTextActivity.class);
+        startActivity(intent);
     }
 }
--- VoiceRecognition/app/src/main/java/unicredit/voicerecognition/VoiceTextActivity.java
+++ VoiceRecognition/app/src/main/java/unicredit/voicerecognition/VoiceTextActivity.java
@@ -1,7 +1,15 @@
 package unicredit.voicerecognition;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by John on 8/1/2016.
  */
-public class VoiceTextActivity {
+public class VoiceTextActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_voice_text);
+    }
 }
