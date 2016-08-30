8a640f065be1093d36d03252ae26ed2dad230b8c
Alex Filiuta <alex.filiuta@gmail.com>
2016-08-30 08:12:24 +0300

    Added Menu Button to Text-Voice GUI
    -Added menu button
    -Fixed some bugs that caused the app to crash

--- VoiceRecognition/app/src/main/java/unicredit/voicerecognition/TextVoiceActivity.java
+++ VoiceRecognition/app/src/main/java/unicredit/voicerecognition/TextVoiceActivity.java
@@ -3,6 +3,8 @@ package unicredit.voicerecognition;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class TextVoiceActivity extends AppCompatActivity {
@@ -11,9 +13,32 @@ public class TextVoiceActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text_voice);
+        this.setTitle("Text to Voice");
     }
 
     public void goBack(View view) {
         super.onBackPressed();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // action with ID action_refresh was selected
+            case R.id.action_mainmenu:
+                super.onBackPressed();
+                break;
+            default:
+                break;
+        }
+
+        return true;
     }
 }
