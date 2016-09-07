df5fdf008e2397c5c76a31f4e3970d1f7a473b7e
Ionut Boicu <ionut.boicu@stud.acs.upb.ro>
2016-09-07 09:31:30 +0300

    Bug solved?
    testing

--- VoiceRecognition/app/src/main/java/unicredit/voicerecognition/TextVoiceActivity.java
+++ VoiceRecognition/app/src/main/java/unicredit/voicerecognition/TextVoiceActivity.java
@@ -23,45 +23,70 @@ import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.widget.Toast;
 
-public class TextVoiceActivity extends AppCompatActivity {
-    TextToSpeech tts;
-    EditText ed;
-    Button butt;
+public class TextVoiceActivity extends Activity implements View.OnClickListener, TextToSpeech.OnInitListener {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    //TTS object
+    private TextToSpeech myTTS;
+    //status check code
+    private int MY_DATA_CHECK_CODE = 0;
+
+    //create the Activity
+    public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text_voice);
-        this.setTitle("Text to Voice");
-        ed = (EditText)findViewById(R.id.playTextBox);
-        butt = (Button)findViewById(R.id.playButton);
-        tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
-            @Override
-            public void onInit(int status) {
-                if (status != TextToSpeech.ERROR) {
-                    tts.setLanguage(Locale.UK);
-                }
-            }
-        });
 
-        butt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            @SuppressWarnings("deprecation")
-            public void onClick(View v) {
-                String speaking = ed.getText().toString();
-                tts.speak(speaking, TextToSpeech.QUEUE_FLUSH, null);
-            }
-        });
+        //check for TTS data
+        Intent checkTTSIntent = new Intent();
+        checkTTSIntent.setAction(TextToSpeech.Engine.ACTION_CHECK_TTS_DATA);
+        startActivityForResult(checkTTSIntent, MY_DATA_CHECK_CODE);
     }
 
-    public void onPause() {
-        if (tts != null) {
-            tts.stop();
-            tts.shutdown();
+    //respond to button clicks
+    public void onClick(View v) {
+
+        //get the text entered
+        EditText enteredText = (EditText)findViewById(R.id.playTextBox);
+        String words = enteredText.getText().toString();
+        speakWords(words);
+    }
+
+    //speak the user text
+    private void speakWords(String speech) {
+
+        //speak straight away
+        myTTS.speak(speech, TextToSpeech.QUEUE_FLUSH, null);
+    }
+
+    //act on result of TTS data check
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == MY_DATA_CHECK_CODE) {
+            if (resultCode == TextToSpeech.Engine.CHECK_VOICE_DATA_PASS) {
+                //the user has the necessary data - create the TTS
+                myTTS = new TextToSpeech(this, this);
+            }
+            else {
+                //no data - install it now
+                Intent installTTSIntent = new Intent();
+                installTTSIntent.setAction(TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA);
+                startActivity(installTTSIntent);
+            }
         }
-        super.onPause();
     }
 
+    //setup TTS
+    public void onInit(int initStatus) {
+
+        //check for successful instantiation
+        if (initStatus == TextToSpeech.SUCCESS) {
+            if(myTTS.isLanguageAvailable(Locale.US)==TextToSpeech.LANG_AVAILABLE)
+                myTTS.setLanguage(Locale.US);
+        }
+        else if (initStatus == TextToSpeech.ERROR) {
+            Toast.makeText(this, "Sorry! Text To Speech failed...", Toast.LENGTH_LONG).show();
+        }
+    }
     public void goBack(View view) {
         super.onBackPressed();
     }
